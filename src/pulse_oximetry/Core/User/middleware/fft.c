441d473cf7687cc305dd32390f79fd11855f0689
khanhnguyenngoc-itr-intern <ngockhanh20003@gmail.com>
2024-10-14 23:11:10 +0700

    update: Create fft header and source files

---
+++ src/pulse_oximetry/Core/User/middleware/fft.c
@@ -0,0 +1,33 @@
+/**
+ * @file       fft.c
+ * @copyright
+ * @license
+ * @version    1.0.0
+ * @date       14/10/2024
+ * @author     Khanh Nguyen Ngoc
+ *
+ * @brief      FFT APIs
+ *
+ * @note       None
+ * @example    None
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "common.h"
+#include "fft.h"
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+
+/* Function definitions ----------------------------------------------- */
+
+/* End of file -------------------------------------------------------- */
---
+++ src/pulse_oximetry/Core/User/middleware/fft.h
@@ -0,0 +1,34 @@
+/**
+ * @file       fft.h
+ * @copyright
+ * @license
+ * @version    1.0.0
+ * @date       14/10/2024
+ * @author     Khanh Nguyen Ngoc
+ *
+ * @brief      FFT APIs
+ *
+ * @note       None
+ * @example    None
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef USER_FFT_FFT_H_
+#define USER_FFT_FFT_H_
+
+/* Includes ----------------------------------------------------------- */
+#include "arm_math.h"
+
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+
+#endif /* USER_FFT_FFT_H_ */
+
+/* End of file -------------------------------------------------------- */
