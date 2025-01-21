1560e3cbe144d79aaa8a6d012309d57d39a964cd
Khanh Nguyen <ngockhanh20003@gmail.com>
2025-01-22 02:11:43 +0700

    update: Matlab tools for labeling peaks

---
+++ dataset_100hz_heart_rate_measure_result/algorithm/real_world_data/analysist/signal_labeling_main.m
@@ -0,0 +1,18 @@
+% Path to the CSV file
+mimic_afib_ppg_filepath = '../data/filtered_ppg_data_100hz.csv';
+
+% Count the number of lines in the file
+fileID = fopen(mimic_afib_ppg_filepath, 'r');
+sample = 0;
+while ~feof(fileID)
+    fgetl(fileID); % Read each line from the file
+    sample = sample + 1;
+end
+fclose(fileID);
+
+mimic_afib_ppg_data = csvread(mimic_afib_ppg_filepath, 0, 0, [0, 0, sample-1, 0]);
+
+signal = double(mimic_afib_ppg_data);
+
+% Call the signal_labeling_ui function with the read signal
+signal_labeling_ui(signal);
